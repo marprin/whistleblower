8846bf73c64c1c441e821b7fcea67d6aee1bf6a8
marprin <marprin93@gmail.com>
2021-08-13 09:08:53 +0700

    Set font header as bold

--- public-web/src/component/header/Header.js
+++ public-web/src/component/header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Header extends React.Component {
 	render() {
 		return (
-			<h3 className="text-5xl text-center text-green-500 my-8">Whistleblower</h3>
+			<h3 className="text-5xl text-center text-green-600 font-bold my-8">Whistleblower</h3>
 		)
 	}
 }
