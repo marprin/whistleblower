ff6c71a04972992305f9b79bdabf9bfa9f4f3eb2
marprin <marprin93@gmail.com>
2021-08-26 18:11:07 +0700

    Implement validation in whistleblower

--- public-backend/src/handler/whistleblower.ts
+++ public-backend/src/handler/whistleblower.ts
@@ -1,7 +1,16 @@
-import { Response, Request } from 'express';
+import { Request, Response } from 'express';
+import { SubmitWhistleblowerEntity } from '../entity/whistleblower';
+import Log from '../lib/logger';
 
+export const submit = async (req: Request, res: Response) => {
+	let payload;
+	try {
+		payload = await SubmitWhistleblowerEntity.validateAsync(req.body);
+	} catch (err) {
+		return res.status(422).json({"errors": err.details});
+	}
 
-export const submit = (req: Request, res: Response) => {
+	
 
-	return res.status(201).json({"message": "The whistle is received"})
+	return res.status(201).json({"message": "The whistle is received"});
 }
